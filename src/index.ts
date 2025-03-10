0b2eef6092110169a3360dce9b7d0efc74a8fd15
Maxime Le Duc <maximeld19@gmail.com>
2025-03-10 20:17:56 +0100

    Change export name and export some types

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,12 @@ import { Flagster } from "./flagster";
 import { NullLocalStorage } from "./localstorage/null-localstorage";
 import { LocalStorage } from "./localstorage/real-localstorage";
 export type { IApi } from "./api/api";
-export type { Config } from "./flagster";
+export type { Config, FlagsterState } from "./flagster";
 export { Flagster } from "./flagster";
 
 const http = new Http();
 const api = new HttpApi(http, "https://api.flagster.fr");
 
-export const flagster = () => new Flagster(api, new LocalStorage());
-export const flagsterSSR = () => new Flagster(api, new NullLocalStorage());
+export const createFlagster = () => new Flagster(api, new LocalStorage());
+export const createFlagsterSSR = () =>
+	new Flagster(api, new NullLocalStorage());
