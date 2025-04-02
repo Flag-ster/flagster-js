4799a22d6ef56b758c31e16750c6b20a9bc16a5c
Maxime Le Duc <maximeld19@gmail.com>
2025-04-02 09:46:24 +0200

    Export useful types

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,9 @@ import { NullFlagsStorage } from "./flags-storage/null-flags-storage";
 export type { IApi } from "./api/api";
 export type { Config, FlagsterState } from "./flagster";
 export { Flagster } from "./flagster";
+export type { IdentityGenerator } from "./identity-generator/identity-generator";
+export type { IdentityStorage } from "./identity-storage/identity-storage";
+export type { FlagsStorage } from "./flags-storage/flags-storage";
 
 const http = new Http();
 const api = new HttpApi(http, "https://api.flagster.fr");
